ac4e7c779942c984260cc422b5e7c1223b8bb9bd
Shaun Guth <shaun.guth@gmail.com>
2012-12-14 02:22:57 -0800

    Initial commit

---
+++ PandoraCapture/PandoraCapture.cs
@@ -0,0 +1,41 @@
+﻿using Fiddler;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+[assembly: Fiddler.RequiredVersion("2.3.5.0")]
+
+namespace PandoraCapture
+{
+    public class PandoraCapture : IAutoTamper
+    {
+        public void AutoTamperResponseBefore(Session oSession)
+        {
+            if (oSession.uriContains("pandora") && oSession.uriContains(".mp3"))
+            {
+                string desktopPath = Environment.GetFolderPath(System.Environment.SpecialFolder.DesktopDirectory);
+                string suggestedFileName = Path.GetFileNameWithoutExtension(oSession.SuggestedFilename);
+                string filePath = Path.Combine(desktopPath, suggestedFileName);
+
+                while (File.Exists(filePath)) {
+                    string name = Path.GetFileNameWithoutExtension(suggestedFileName);
+                    string ext = Path.GetExtension(suggestedFileName);
+                    name += "_";
+                    filePath = Path.Combine(desktopPath, Path.ChangeExtension(name, ext));
+                }
+
+                File.WriteAllBytes(filePath, oSession.responseBodyBytes);
+            }
+
+            return;
+        }
+
+        public void OnLoad() {}
+        public void OnBeforeUnload() {}
+        public void AutoTamperResponseAfter(Session oSession) {}
+        public void AutoTamperRequestBefore(Session oSession) { }
+        public void AutoTamperRequestAfter(Session oSession) { }
+        public void OnBeforeReturningError(Session oSession) { }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 pandoracapture
 ==============
 
-A Fiddler2 extension that captures Pandora mp3 files
+A Fiddler2 extension that automatically captures Pandora mp3 files to your desktop
+
+To use:
+- compile the solution
+- put the resulting dll in My Documents\Fiddler2\Scripts
+- start Fiddler2
+- start Pandora
+- wait for files to be saved to your desktop
+
+The files don't contain any metadata, so you may want to use Picard or a similar tool to help identify them.
+A future improvement to ths project would be to look at the metadata requests to Pandora and collate them with the mp3s
